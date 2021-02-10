15bd33efb1459048afc438c67e10673998279b5e
RohanCarlo <69616556+RohanCarlo@users.noreply.github.com>
2021-02-10 09:34:16 +0530

    Update tromboloid_with_1_function.c

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -1,11 +1,18 @@
 //Write a program to find the volume of a tromboloid using one function
-#include<stdio.h>
 int main()
 {
-float h,b,d,vol;
-printf("Enter height,breadth,depth:");
-scanf("%f%f%f",&h,&b,&d);
-vol=((h*d)+d)/(3*b);
-printf("Volume=%f",vol);
+	float h,b,d,vol;
+printf(“Enter height,breadth,depth:”);
+scanf(“%f%f%f”,&h,&b,&d);
+      if(h>0 & b>0 & d>0)
+{
+vol=((h*d*b)+(d/b))/3;
+printf(“Volume=%f”,vol);
+}
+      else
+{
+   printf("Incorrect input");
+}
 return 0;
 }
+  
